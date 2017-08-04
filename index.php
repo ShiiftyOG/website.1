4effe8292b73b0a586129fafa67246d548989436
C3ypt1c <c3ypt1c@gmail.com>
2017-08-04 19:12:13 +0100

    Added file structure

--- index.php
+++ index.php
@@ -1,10 +1,14 @@
-<!DOCTYPE HTML>
-<HTML>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>MaidToClean</title>
+	<link rel="stylesheet" type="text/css" href="style/style.css">
+	<link rel="stylesheet" type="text/css" href="style/mobi.css" media="screen and (max-aspect-ratio: 1/1)">
+	<link rel="stylesheet" type="text/css" href="style/pc.css" media="screen and (min-aspect-ratio: 1/1)">
+</head>
+<body>
+	<h1 id="title">MaidToClean</h1>
+	<p>Paragraph</p>
 
-<HEAD>
-</HEAD>
-
-<BODY>
-</BODY>
-
-</HTML>
+</body>
+</html>
