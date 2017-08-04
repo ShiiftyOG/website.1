74fad406dd9ea3a27cfe7198ff03aa309281603f
C3ypt1c <c3ypt1c@gmail.com>
2017-08-04 19:40:56 +0100

    Fixed Error

--- index.php
+++ index.php
@@ -7,11 +7,15 @@
 	<link rel="stylesheet" type="text/css" href="style/pc.css" media="screen and (min-aspect-ratio: 1/1)">
 </head>
 <body>
+	
 	<h1 id="title">MaidToClean</h1>
+
 	<p>Paragraph</p>
 
-<<<<<<< HEAD
 </body>
+<<<<<<< HEAD
+</html>
+=======
 </html>
 =======
 <HEAD>
@@ -19,8 +23,8 @@
 
 <BODY>
 hello World :/
-
 </BODY>
 
 </HTML>
 >>>>>>> 4a0de581550806aab58248a200789942afd37ecd
+>>>>>>> 2ad03efde980122cba239469b4172462cfd0873d
