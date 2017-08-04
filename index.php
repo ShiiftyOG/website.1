2eb23e190f44ff56d0b705a4a187f7091d98feb6
C3ypt1c <c3ypt1c@gmail.com>
2017-08-04 20:21:31 +0100

    Made all sections

--- index.php
+++ index.php
@@ -1,15 +1,33 @@
 <!DOCTYPE html>
 <html>
-<head>
-	<title>MaidToClean</title>
-	<link rel="stylesheet" type="text/css" href="style/style.css">
-	<link rel="stylesheet" type="text/css" href="style/mobi.css" media="screen and (max-aspect-ratio: 1/1)">
-	<link rel="stylesheet" type="text/css" href="style/pc.css" media="screen and (min-aspect-ratio: 1/1)">
-</head>
-<body>
-	
-	<h1 id="title">MaidToClean</h1>
+	<head>
+		<title>MaidToClean</title>
+		<link rel="stylesheet" type="text/css" href="style/style.css">
+		<link rel="stylesheet" type="text/css" href="style/mobi.css" media="screen and ( (max-aspect-ratio: 1/1) or (aspect-ratio: 1/1) )">
+		<link rel="stylesheet" type="text/css" href="style/pc.css" media="screen and (min-aspect-ratio: 1/1)">
+	</head>
+	<body>
+		<div id="mainWrapper">
+			<div class="spanWrapper">
+				<h1>MaidToClean</h1>
+			</div>
 
-	<p>Paragraph</p>
+			<img src="style/imgs/logo.bmp" alt="">
 
-</body>
+			<div class="spanWrapper">
+				<h3>About us</h3>
+				<p>Paragraph</p>
+			</div>
+
+			<div class="spawWrapper">
+				<h3>Contact us</h3>
+				<p>Paragraph info</p>
+			</div>
+
+			<div id="footer">
+				<h6>Some stuff</h6>
+			</div>
+
+		</div>
+	</body>
+</html>
