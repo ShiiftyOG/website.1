ae98387271118acda82b0aa38c4af0c329424750
C3ypt1c <c3ypt1c@gmail.com>
2017-08-07 20:13:16 +0100

    Final compability bugfix

--- config.php
+++ config.php
@@ -14,7 +14,7 @@ $size = "200";
 $loadingMessage = "Website is loading";
 
 //Name for the website
-$title= "This is the title";
+$title= "Sample Text";
 
 //About Us (//Thank you http://www.lipsum.com/)
 $aboutUs ="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer eu elit eu enim semper auctor at at risus. Donec tristique fringilla est at rhoncus. Maecenas iaculis ut lacus vel ultrices. Phasellus nec elit quis justo cursus congue. Ut nec odio ut risus condimentum pretium. Vestibulum et viverra massa, et porttitor arcu. Aliquam nec sem ut sem accumsan eleifend nec sed sapien. Quisque vulputate odio nec ante hendrerit, sit amet dictum ex bibendum. Ut in purus et ligula iaculis porta. Sed vitae felis scelerisque, rutrum libero viverra, malesuada sem. Proin pharetra scelerisque dui, eget tempor justo tincidunt ac.";
