e4b51d07937f460f90319704d35094bb5f3e7bba
C3ypt1c <c3ypt1c@gmail.com>
2017-08-10 21:42:18 +0100

    Don't output password when sending form

--- send.php
+++ send.php
@@ -33,9 +33,6 @@ if ( $a == "" && $system == "linux" ) {
 		<title>Thank you</title>
 	</head>
 	<body>
-	<p>	Output: <?php print_r ( $a ); ?>
-		Input: 	<?php echo ( "$python$file" ); ?>
-	</p>
 		<p>Your message has been sent!<br><a href="/">Click here</a> to go back</p>
 	</body>
 </html>
